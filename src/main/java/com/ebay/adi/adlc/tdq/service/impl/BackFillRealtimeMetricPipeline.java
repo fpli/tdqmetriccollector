b5903277117ed2d4d15d8b1623b6fa6a185d524d
fangpli <fangpli@ebay.com>
2023-11-01 15:04:29 +0800

    add pronto and MySQL config parameters

---
+++ src/main/java/com/ebay/adi/adlc/tdq/service/impl/BackFillRealtimeMetricPipeline.java
@@ -0,0 +1,39 @@
+package com.ebay.adi.adlc.tdq.service.impl;
+
+import com.ebay.adi.adlc.tdq.service.BackFillRealtimeMetricOption;
+import com.ebay.adi.adlc.tdq.util.SparkSessionStore;
+import org.apache.commons.cli.*;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class BackFillRealtimeMetricPipeline extends BasePipeline<BackFillRealtimeMetricOption> {
+
+    @Override
+    public BackFillRealtimeMetricOption parseCommand(String[] args) {
+        BackFillRealtimeMetricOption backFillRealtimeMetricOption = new BackFillRealtimeMetricOption();
+        DefaultParser defaultParser = getDefaultParser();
+        Options options = new Options();
+        Option startOpt = new Option("start", "start", true, "the start time of the range");
+        Option endOpt = new Option("end", "end", true, "the end time of the range");
+        // ...
+        try {
+            CommandLine commandLine = defaultParser.parse(options, args);
+            String start = commandLine.getOptionValue(startOpt);
+            backFillRealtimeMetricOption.setStart(start);
+            String end = commandLine.getOptionValue(endOpt);
+            backFillRealtimeMetricOption.setEnd(end);
+            // ...
+        } catch (ParseException e) {
+            logger.error("parsing command line arguments {} occurred some errors:", args, e);
+            throw new RuntimeException(e);
+        }
+        return backFillRealtimeMetricOption;
+    }
+
+    @Override
+    public void process(BackFillRealtimeMetricOption backFillRealtimeMetricOption) {
+        SparkSession spark = SparkSessionStore.getInstance().getSparkSession();
+        Dataset<Row> dataset = spark.sql("");
+    }
+}
--- src/main/java/com/ebay/adi/adlc/tdq/util/PipelineFactory.java
+++ src/main/java/com/ebay/adi/adlc/tdq/util/PipelineFactory.java
@@ -1,11 +1,14 @@
 package com.ebay.adi.adlc.tdq.util;
 
 import com.ebay.adi.adlc.tdq.service.BaseOption;
-import com.ebay.adi.adlc.tdq.service.impl.BasePipeline;
-import com.ebay.adi.adlc.tdq.service.impl.MetricCollectorPipeline;
-import com.ebay.adi.adlc.tdq.service.impl.PageProfilingPipeline;
-import com.ebay.adi.adlc.tdq.service.impl.TagProfilingPipeline;
+import com.ebay.adi.adlc.tdq.service.impl.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,6 +21,7 @@ public class PipelineFactory {
             synchronized (PipelineFactory.class) {
                 if (null == instance) {
                     instance = new PipelineFactory();
+                    instance.init();
                 }
             }
         }
@@ -29,12 +33,42 @@ public class PipelineFactory {
         pipelineMap.put("pageProfiling", new PageProfilingPipeline());
         pipelineMap.put("metricCollector", new MetricCollectorPipeline());
         pipelineMap.put("tagProfiling", new TagProfilingPipeline());
+        pipelineMap.put("backFillRealtimeMetric", new BackFillRealtimeMetricPipeline());
     }
 
     private Map<String, BasePipeline<? extends BaseOption>> pipelineMap;
 
     public BasePipeline<? extends BaseOption> findPipeline(String bizId) {
         return pipelineMap.get(bizId);
+    }
+
+    public void init() {
+        try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("application.yaml")) {
+            ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory()).configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            Map<String, Object> properties = objectMapper.readValue(inputStream, new TypeReference<Map<String, Object>>() {});
+            flatMap(properties);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void flatMap(Map<String, Object> map){
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            if (entry.getValue() instanceof Map){
+                recurseReplace(map, entry.getKey(), (Map<String, Object>) entry.getValue());
+            }
+        }
+    }
+
+    private void recurseReplace(Map<String, Object> dest, String key, Map<String, Object> src){
+        for (Map.Entry<String, Object> entry : src.entrySet()) {
+            String k = key + "." + entry.getKey();
+            if (entry.getValue() instanceof Map){
+                recurseReplace(dest, k, (Map<String, Object>) entry.getValue());
+            } else {
+                dest.put(k, entry.getValue());
+            }
+        }
     }
 
 }
