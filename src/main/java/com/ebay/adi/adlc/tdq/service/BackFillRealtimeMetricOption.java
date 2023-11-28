d0ebd3b7860b61eb9169c41decfdc720d43d3ab0
fangpli <fangpli@ebay.com>
2023-11-28 15:12:58 +0800

    fix bugs

--- src/main/java/com/ebay/adi/adlc/tdq/service/BackFillRealtimeMetricOption.java
+++ src/main/java/com/ebay/adi/adlc/tdq/service/BackFillRealtimeMetricOption.java
@@ -2,23 +2,15 @@ package com.ebay.adi.adlc.tdq.service;
 
 public class BackFillRealtimeMetricOption extends BaseOption {
     // yyyy-MM-dd HH:mm:ss
-    private String start;
+    private String date;
 
-    private String end;
-
-    public String getStart() {
-        return start;
+    @Override
+    public String getDate() {
+        return date;
     }
 
-    public void setStart(String start) {
-        this.start = start;
-    }
-
-    public String getEnd() {
-        return end;
-    }
-
-    public void setEnd(String end) {
-        this.end = end;
+    @Override
+    public void setDate(String date) {
+        this.date = date;
     }
 }
--- src/main/java/com/ebay/adi/adlc/tdq/service/impl/BackFillRealtimeMetricPipeline.java
+++ src/main/java/com/ebay/adi/adlc/tdq/service/impl/BackFillRealtimeMetricPipeline.java
@@ -4,7 +4,6 @@ import com.ebay.adi.adlc.tdq.service.BackFillRealtimeMetricOption;
 import com.ebay.adi.adlc.tdq.service.BaseOption;
 import com.ebay.adi.adlc.tdq.util.PipelineFactory;
 import com.ebay.adi.adlc.tdq.util.SparkSessionStore;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
@@ -12,16 +11,16 @@ import org.apache.commons.cli.Parser;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
-import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.reindex.DeleteByQueryRequest;
+import org.elasticsearch.script.Script;
+import org.elasticsearch.script.ScriptType;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -40,15 +39,12 @@ public class BackFillRealtimeMetricPipeline extends BasePipeline<BackFillRealtim
         BackFillRealtimeMetricOption backFillRealtimeMetricOption = new BackFillRealtimeMetricOption();
         Parser defaultParser = getDefaultParser();
         Options options = new Options();
-        options.addOption("start", "start", true, "the start time of the range");
-        options.addOption("end", "end", true, "the end time of the range");
+        options.addOption("d", "date", true, "the date will be filled");
         // ...
         try {
             CommandLine commandLine = defaultParser.parse(options, args);
-            String start = commandLine.getOptionValue("start");
-            backFillRealtimeMetricOption.setStart(start);
-            String end = commandLine.getOptionValue("end");
-            backFillRealtimeMetricOption.setEnd(end);
+            String date = commandLine.getOptionValue("d");
+            backFillRealtimeMetricOption.setDate(date);
             // ...
         } catch (ParseException e) {
             logger.error("parsing command line arguments {} occurred some errors:", args, e);
@@ -61,24 +57,58 @@ public class BackFillRealtimeMetricPipeline extends BasePipeline<BackFillRealtim
     public void process(BaseOption parameter) {
         BackFillRealtimeMetricOption backFillRealtimeMetricOption = (BackFillRealtimeMetricOption) parameter;
         SparkSession spark = SparkSessionStore.getInstance().getSparkSession();
-        String start = backFillRealtimeMetricOption.getStart();
-        String end = backFillRealtimeMetricOption.getEnd();
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime localDateTime = LocalDateTime.parse(start, dateTimeFormatter);
-        LocalDateTime localDateTime2 = LocalDateTime.parse(end, dateTimeFormatter);
-        int hour = localDateTime.getHour();
-        int hour1 = localDateTime2.getHour();
+        String date = backFillRealtimeMetricOption.getDate();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        LocalDateTime localDateTime = LocalDateTime.parse(date, dateTimeFormatter);
         LocalDate localDate = localDateTime.toLocalDate();
-        String dt = DateTimeFormatter.ofPattern("yyyyMMdd").format(localDate);
-        String dtt = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(localDate);
-        String sql = "SELECT hr, pageId, count(1) as cnt FROM ubi_w.stg_ubi_event_dump_w WHERE dt = '%s' AND type = 'nonbot' AND hr BETWEEN %d and %d GROUP BY hr, pageId ORDER BY hr ASC";
-        String actualSql = String.format(sql, dt, hour, hour1);
+        String dt = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(localDate);
+        //String sql = "SELECT hr, pageId, count(1) as cnt FROM ubi_w.stg_ubi_event_dump_w WHERE dt = '%s' AND type = 'nonbot' AND hr BETWEEN %d and %d GROUP BY hr, pageId ORDER BY hr ASC";
+        //String actualSql = String.format(sql, dt, hour, hour1);
+        //logger.info(actualSql);
+        //Dataset<Row> dataset = spark.sql(actualSql);
+        //List<Row> rows = dataset.collectAsList();
+        String sql = " SELECT\n" +
+                "  t1.hr,\n" +
+                "  t1.pageid,\n" +
+                "  t1.cnt,\n" +
+                "  nvl(t2.late_cnt, 0) late_event_cnt\n" +
+                "FROM\n" +
+                "  (\n" +
+                "   SELECT\n" +
+                "\t  hr,\n" +
+                "\t  PAGE_ID pageid,\n" +
+                "\t  count(PAGE_ID) cnt\n" +
+                "\tFROM\n" +
+                "\t  (\n" +
+                "\t\tSELECT\n" +
+                "\t\t  PAGE_ID,\n" +
+                "\t\t  hour(EVENT_TIMESTAMP) hr\n" +
+                "\t\tFROM\n" +
+                "\t\t  UBI_V.UBI_EVENT\n" +
+                "\t\tWHERE\n" +
+                "\t\t  SESSION_START_DT = '%s'\n" +
+                "\t  ) t\n" +
+                "\t  GROUP BY hr, PAGE_ID\n" +
+                "  ) t1\n" +
+                "  LEFT JOIN (\n" +
+                "    SELECT\n" +
+                "      hr,\n" +
+                "      PAGEID,\n" +
+                "      count(PAGEID) late_cnt\n" +
+                "    FROM\n" +
+                "      ubi_w.tdq_rt_late_sojevent\n" +
+                "    WHERE\n" +
+                "      DT = '%s'\n" +
+                "      GROUP BY hr, PAGEID\n" +
+                "  ) t2 ON t1.pageId = t2.PAGEID\n" +
+                "  AND t1.hr = t2.hr";
+        String actualSql = String.format(sql, dt, dt);
         logger.info(actualSql);
         Dataset<Row> dataset = spark.sql(actualSql);
         List<Row> rows = dataset.collectAsList();
         try {
             RestHighLevelClient restHighLevelClient = PipelineFactory.getInstance().getRestHighLevelClient();
-            String index = "my-index_bak";
+            String index = "prod.metric.rt.page";
             GetIndexRequest getIndexRequest = new GetIndexRequest(index);
             boolean exists = restHighLevelClient.indices().exists(getIndexRequest, RequestOptions.DEFAULT);
             if (!exists){
@@ -91,40 +121,33 @@ public class BackFillRealtimeMetricPipeline extends BasePipeline<BackFillRealtim
                     logger.error("index: {} creation was failed!", index);
                 }
             }
-            String metric_key = "hourly_event_cnt";
-            String create_time = LocalDateTime.now().toString();
-            long metric_time = System.nanoTime();
             LongAdder errorCounter = new LongAdder();
-            ObjectMapper objectMapper = new ObjectMapper();
             Base64.Encoder encoder = Base64.getEncoder();
             rows.stream().parallel().forEach(row -> {
-                String hr = row.getString(0);
-                int page_id = row.getInt(1);
-                long event_cnt = row.getLong(2);
-                DeleteByQueryRequest deleteByQueryRequest = new DeleteByQueryRequest(index);
-                deleteByQueryRequest.setConflicts("proceed");
-                BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
-                boolQueryBuilder.must(QueryBuilders.termQuery("metric_key", metric_key));
-                boolQueryBuilder.must(QueryBuilders.termQuery("dt", dt));
-                boolQueryBuilder.must(QueryBuilders.termQuery("page_id", page_id));
-                boolQueryBuilder.must(QueryBuilders.termQuery("hr", hr));
-
-                deleteByQueryRequest.setQuery(boolQueryBuilder);
                 try {
-                    restHighLevelClient.deleteByQuery(deleteByQueryRequest, RequestOptions.DEFAULT);
-                    Map<String, Object> map = new HashMap<>();
-                    map.put("event_cnt", event_cnt);
-                    map.put("create_time", create_time);
-                    map.put("hr", hr);
-                    map.put("metric_time", metric_time);
-                    map.put("dt", dt);
-                    map.put("metric_key", metric_key);
-                    map.put("page_id", page_id);
-                    IndexRequest indexRequest = new IndexRequest(index);
-                    indexRequest.id(encoder.encodeToString((page_id + "." + dtt + hr).getBytes()));
-                    String payload = objectMapper.writeValueAsString(map);
-                    indexRequest.source(payload, XContentType.JSON);
-                    restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+                    String hr = row.getString(0);
+                    int page_id = row.getInt(1);
+                    long event_cnt = row.getLong(2);
+                    long late_event_cnt = row.getLong(3);
+                    //IndexRequest indexRequest = new IndexRequest(index);
+                    String id = encoder.encodeToString((page_id + "." + dt + hr).getBytes());
+                    //indexRequest.id(id);
+                    GetRequest getRequest = new GetRequest(index, id);
+                    getRequest.fetchSourceContext(new FetchSourceContext(false));
+                    getRequest.storedFields("_none_");
+                    boolean exist = restHighLevelClient.exists(getRequest, RequestOptions.DEFAULT);
+                    if (exist) {
+                        UpdateRequest updateRequest = new UpdateRequest(index, id);
+                        Map<String, Object> parameters = new HashMap<>();
+                        parameters.put("late_event_cnt", late_event_cnt);
+                        parameters.put("batch_event_cnt", event_cnt);
+
+                        Script script = new Script(ScriptType.INLINE, "painless", "ctx._source.acc_rt_event_cnt = ctx._source.rt_event_cnt +  params.late_event_cnt;ctx._source.batch_event_cnt = params.batch_event_cnt", parameters);
+                        updateRequest.script(script);
+
+                        //indexRequest.source(payload, XContentType.JSON);
+                        restHighLevelClient.update(updateRequest, RequestOptions.DEFAULT);
+                    }
                 } catch (IOException e) {
                     logger.error("errors occurred in deleting index: ", e);
                     errorCounter.increment();
