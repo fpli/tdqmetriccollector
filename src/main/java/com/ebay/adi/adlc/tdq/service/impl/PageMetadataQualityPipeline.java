9154e63f3aa551b4191ed268e2cddad59617ad07
fangpli <fangpli@ebay.com>
2024-01-11 15:00:11 +0800

    unwanted page metadata quality

--- src/main/java/com/ebay/adi/adlc/tdq/service/impl/PageMetadataQualityPipeline.java
+++ src/main/java/com/ebay/adi/adlc/tdq/service/impl/PageMetadataQualityPipeline.java
@@ -82,7 +82,7 @@ public class PageMetadataQualityPipeline extends BasePipeline<PageMetadataOption
         saveToMySQL(pagePoolMappingList);
 
         Dataset<Row> rowDataset = spark.createDataFrame(pagePoolMappingList, PagePoolMapping.class);
-        rowDataset.write().mode(SaveMode.Overwrite).option("path", "viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/tdq/tdq_page_metadata_quality_w").insertInto("ubi_w.tdq_page_metadata_quality_w");
+        rowDataset.write().partitionBy("dt").mode(SaveMode.Overwrite).option("path", "viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/tdq/tdq_page_metadata_quality_w").insertInto("ubi_w.tdq_page_metadata_quality_w");
     }
 
     private void saveToMySQL(List<PagePoolMapping> pagePoolMappingList) {
